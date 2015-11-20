a9868447b027c3710e82fe25e0e09a681389ea8b
toklok <joecurt85@gmail.com>
2015-11-19 20:53:40 -0500

    Upload the scaffolding to my Alexa Skill

---
+++ index.js
@@ -0,0 +1,27 @@
+const AlexaSkill = require('./AlexaSkill');
+
+const APP_ID = undefined; //replace with Alexa key
+
+
+//Call Light is a child of Alexa Skill.
+
+let CallLight = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+//Extend AlexaSkill
+
+CallLight.prototype.eventHandlers.onSessionStarted = function (sessionStorageRequest, session) {
+    console.log("CallLight onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+
+    // any session init logic would go here
+};
+
+CallLight.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+
+    console.log("CallLight onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    console.log(response);
+
+    //On launch, we can do stuff inside here.
+};
