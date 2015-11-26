98575496df48b74c9ce18c8deeca1c9f2ddd01eb
toklok <joecurt85@gmail.com>
2015-11-26 17:29:52 -0500

    Update index.js file and build out Alexa intent's for AWS IoT project

--- AlexaSkills/index.js
+++ AlexaSkills/index.js
@@ -5,13 +5,13 @@ var AlexaSkill = require('./AlexaSkill');
 var APP_ID = undefined; //replace with Alexa key
 
 
-//Call Light is a child of Alexa Skill.
+//Call Light a constructor is a child of Alexa Skill.
 
 var CallLight = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
-//Classical inheritence
+//Classical Inheritance
 
 CallLight.prototype = Object.create(AlexaSkill.prototype);
 CallLight.prototype.constructor = CallLight;
@@ -29,4 +29,37 @@ CallLight.prototype.eventHandlers.onLaunch = function (launchRequest, session, r
     console.log(response);
 
     //On launch, we can do stuff inside here.
+};
+
+CallLight.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+
+    // any session cleanup logic would go here
+};
+
+CallLight.prototype.intentHandlers = {
+
+    "getHelp": function (intent, session, response) {
+
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+
+        var speechText = "You can say get help and the department you would like service from";
+
+        var repromptText = "Please tell me the department you would like to contact";
+
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+
+        var repromptOutput = {
+            speech: repromptText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
+
 };
