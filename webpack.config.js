a669ede264ee8f71fc1ecb3d9186f5b4c034958b
Joseph Curtis <joecurt85@gmail.com>
2015-12-03 20:05:20 -0500

    start using webpack, and mocha for tests after reading an article that inspired me to use ES6

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+
+module.exports = {
+
+    output: {
+        path: path.join(__dirname, "dist"),
+        library: "[name]",
+        libraryTarget: "commonjs2",
+        filename: "[name].js"
+    },
+
+    target: "node",
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['syntax-flow', 'transform-flow-strip-types']
+                }
+
+            },
+
+            {
+                test: /\.json$/,
+                loader: 'json'
+            }
+        ]
+    }
+
+};
